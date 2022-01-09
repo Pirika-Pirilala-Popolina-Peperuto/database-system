f0384232e484bca15262a08919af2ebd0318870f
txya900619 <wayne900619@gmail.com>
2022-01-09 23:34:44 +0800

    Add cors to api

--- pkg/chi.go
+++ pkg/chi.go
@@ -6,12 +6,13 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"go.uber.org/fx"
 )
 
 func NewRouter(lifecycle fx.Lifecycle) (*chi.Mux, error) {
 	r := chi.NewRouter()
-	r.Use(middleware.Logger, middleware.Recoverer)
+	r.Use(middleware.Logger, middleware.Recoverer, cors.AllowAll().Handler)
 
 	server := &http.Server{
 		Addr:    ":80",
